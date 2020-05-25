521e918e8c258608e4de4f2aa29f112b3108c97b
sekharweb <sekhar.webdg@gmail.com>
2020-05-25 22:56:34 +0530

    routing files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import Blog from './containers/Blog/Blog';
 
